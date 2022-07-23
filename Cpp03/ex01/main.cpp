09888cf6a2c378d94d4c015ed8261b598889e3f6
asalek <ayoub.salek8599@gmail.com>
2022-07-23 18:53:43 +0100

    update Cpp03/ex02 add a test

--- Cpp03/ex01/main.cpp
+++ Cpp03/ex01/main.cpp
@@ -6,10 +6,12 @@ int main()
 	ClappTrapp	asalek("asalek");
 	ClappTrapp	AyoubSalek(asalek);
 	ScavTrap	scav;
+	ClappTrapp	*c = &scav;  //anything ClappTrapp can do ScavTrap can do too
 
 	asalek.attack("Hamza Adaoud");
 	asalek.beRepaired(6);
 	asalek.takeDamage(15);
+	c->setName("**");
 	scav.attack("ZooOooZ");
 	scav.GuardGate();
 }

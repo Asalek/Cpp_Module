af9ba144bcc1a12521bb4871df560466ccb76409
asalek <ayoub.salek8599@gmail.com>
2022-07-25 16:13:38 +0100

    update: Cpp04/ex02 explain in comments the pureVirtual functions

--- Cpp04/ex02/Animal.h
+++ Cpp04/ex02/Animal.h
@@ -17,7 +17,9 @@ class Animal   //abstract class have at least one pure VF
 		Animal &operator=(const Animal &animal);
 		virtual ~Animal();
 		string	getType()const;
-		virtual void	makeSound()const = 0;  //pure Virtual Function
+		virtual void	makeSound()const = 0;  //pure Virtual Function //any class that inheret from\
+												 animal will have to provide it's own implimentation \
+												// makeSound=0 force every derived class to implement her own makeSound();
 };
 
 #endif
